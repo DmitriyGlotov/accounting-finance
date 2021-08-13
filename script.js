b57baf722f9e63da542b1944a3f256927498d6ba
DmitriyGloyov <dglotov.exceedteam@gmail.com>
2021-08-13 11:03:26 +0300

    creation of structure

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonAdd = document.createElement('button');
+buttonAdd.className = 'button-add';
+buttonAdd.textContent = 'Добавить';
+
+const mainElemets = document.querySelector('.main-elements');
+mainElemets.appendChild(buttonAdd);
